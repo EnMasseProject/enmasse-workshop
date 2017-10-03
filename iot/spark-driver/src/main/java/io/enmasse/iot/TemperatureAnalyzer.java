6358be0ba0015a0f0265c73268efa4bb08d26358
Paolo Patierno <ppatierno@live.com>
2017-10-03 17:02:21 +0200

    Developed Spark driver application

--- iot/spark-driver/src/main/java/io/enmasse/iot/TemperatureAnalyzer.java
+++ iot/spark-driver/src/main/java/io/enmasse/iot/TemperatureAnalyzer.java
@@ -16,8 +16,28 @@
 
 package io.enmasse.iot;
 
+import io.vertx.core.Vertx;
+import io.vertx.proton.ProtonClient;
+import io.vertx.proton.ProtonConnection;
+import io.vertx.proton.ProtonHelper;
+import io.vertx.proton.ProtonSender;
+import org.apache.qpid.proton.amqp.Binary;
+import org.apache.qpid.proton.amqp.messaging.AmqpValue;
+import org.apache.qpid.proton.amqp.messaging.Data;
+import org.apache.qpid.proton.amqp.messaging.Section;
+import org.apache.qpid.proton.message.Message;
+import org.apache.spark.SparkConf;
+import org.apache.spark.broadcast.Broadcast;
+import org.apache.spark.storage.StorageLevel;
+import org.apache.spark.streaming.Duration;
+import org.apache.spark.streaming.amqp.AMQPUtils;
+import org.apache.spark.streaming.api.java.JavaDStream;
+import org.apache.spark.streaming.api.java.JavaReceiverInputDStream;
+import org.apache.spark.streaming.api.java.JavaStreamingContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import scala.Option;
+import scala.Some;
 
 /**
  * Sample Spark driver for getting temperature values from sensor
@@ -27,7 +47,129 @@ public class TemperatureAnalyzer {
 
     private static final Logger log = LoggerFactory.getLogger(TemperatureAnalyzer.class);
 
-    public static void main(String[] args) {
+    private static final String APP_NAME = "TemperatureAnalyzer";
+    private static final Duration BATCH_DURATION = new Duration(1000);
 
+    private static final String CHECKPOINT_DIR = "/tmp/spark-streaming-amqp";
+
+    private static String host = "localhost";
+    private static int port = 5672;
+    private static String username = null;
+    private static String password = null;
+    private static String sourceAddress = "temperature";
+    private static String destinationAddress = "max";
+
+    public static void main(String[] args) throws InterruptedException {
+
+        // getting AMQP messaging service connection information
+        String messagingServiceHost = System.getenv("MESSAGING_SERVICE_HOST");
+        if (messagingServiceHost != null) {
+            host = messagingServiceHost;
+        }
+        String messagingServicePort = System.getenv("MESSAGING_SERVICE_PORT");
+        if (messagingServicePort != null) {
+            port = Integer.valueOf(messagingServicePort);
+        }
+        log.info("AMQP messaging service hostname {}:{}", host, port);
+
+        // getting credentials for authentication
+        username = System.getenv("USERNAME");
+        password = System.getenv("PASSWORD");
+        log.info("Credentials {}/{}", username, password);
+
+        JavaStreamingContext ssc = JavaStreamingContext.getOrCreate(CHECKPOINT_DIR, TemperatureAnalyzer::createStreamingContext);
+
+        ssc.start();
+        ssc.awaitTermination();
+    }
+
+    private static JavaStreamingContext createStreamingContext() {
+
+        SparkConf conf = new SparkConf().setAppName(APP_NAME);
+        //conf.setMaster("local[2]");
+        conf.set("spark.streaming.receiver.writeAheadLog.enable", "true");
+
+        JavaStreamingContext ssc = new JavaStreamingContext(conf, BATCH_DURATION);
+        ssc.checkpoint(CHECKPOINT_DIR);
+
+        JavaReceiverInputDStream<Integer> receiveStream =
+                AMQPUtils.createStream(ssc, host, port,
+                        Option.apply(username), Option.apply(password), sourceAddress,
+                        message -> {
+
+                            Section section = message.getBody();
+                            if (section instanceof AmqpValue) {
+                                int temp = Integer.valueOf(((AmqpValue) section).getValue().toString());
+                                return new Some<>(temp);
+                            } else if (section instanceof Data) {
+                                Binary data = ((Data)section).getValue();
+                                int temp = Integer.valueOf(new String(data.getArray()));
+                                return new Some<>(temp);
+                            } else {
+                                return null;
+                            }
+
+                        }, StorageLevel.MEMORY_ONLY());
+
+        //max.print();
+
+        // get maximum temperature in a window
+        JavaDStream<Integer> max = receiveStream.reduceByWindow(
+                (a,b) -> {
+
+                    if (a > b)
+                        return a;
+                    else
+                        return b;
+
+                }, new Duration(5000), new Duration(5000));
+
+        Broadcast<String> messagingHost = ssc.sparkContext().broadcast(host);
+        Broadcast<Integer> messagingPort = ssc.sparkContext().broadcast(port);
+
+        max.foreachRDD(rdd -> {
+
+            rdd.foreach(record -> {
+
+                Vertx vertx = Vertx.vertx();
+                ProtonClient client = ProtonClient.create(vertx);
+
+                log.info("Connecting to messaging ...");
+                client.connect(messagingHost.value(), messagingPort.getValue(), done -> {
+
+                    if (done.succeeded()) {
+
+                        log.info("... connected to {}:{}", messagingHost.value(), messagingPort.getValue());
+
+                        ProtonConnection connection = done.result();
+                        connection.open();
+
+                        ProtonSender sender = connection.createSender(destinationAddress);
+                        sender.open();
+
+                        Message message = ProtonHelper.message();
+                        message.setAddress(destinationAddress);
+                        message.setBody(new Data(new Binary(record.toString().getBytes())));
+
+                        log.info("Sending {} ...", record);
+                        sender.send(message, delivery -> {
+
+                            log.info("... message sent");
+                            sender.close();
+                            connection.close();
+                            vertx.close();
+                        });
+
+                    } else {
+
+                        log.error("Error on AMQP connection for sending", done.cause());
+                    }
+
+                });
+
+            });
+        });
+
+        return ssc;
     }
 }
